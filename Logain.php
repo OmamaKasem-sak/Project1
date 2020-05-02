cbed4594029a97580648b60adfb5dfd1b05ba334
Omama Kasem-sak <omamakasem33@gmail.com>
2020-05-02 17:50:38 +0300

    first commit

---
+++ Logain.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<form action="logain1.php" method="post">
+<b> Name:</b>
+ <input type="txt" name="name"><br>
+<b> Password </b>
+  <input type="password" name="password"><br>
+<b>E-mail:</b>
+  <input type="text" name="email"><br>
+  
+<input type="submit"  name="bader" valu="gonder">
+
+  </form>
+ 
+
+</body>
+</html>
---
+++ logain1.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+<table border =1 width =500 height=30>
+<tr>
+<td  width =200 ><b>Name</b></td>
+<td><?php echo $_POST["name"];?></td>
+</tr>
+<tr>
+<td  width =200 ><b>Password</b></td>
+<td><?php echo $_POST["password"];?></td>
+</tr>
+<tr>
+<td  width =200 ><b>E-mail</b></td>
+<td><?php echo $_POST["email"];?></td>
+</tr>
+</table>
+
+
+</body>
+</html>
