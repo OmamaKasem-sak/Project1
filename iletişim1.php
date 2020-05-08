4dece06add7ea9b9c171e2609628ddd9ce4c3c67
Omama Kasem-sak <omamakasem33@gmail.com>
2020-05-08 16:37:54 +0300

    fourth commit

---
+++ iletişim1.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<meta charset="utf-8">
+</head>
+
+<body>
+
+<td><b>Adınız ve Soyadınız:</b></td>
+    <td><?php echo $_POST["ad-soyad"];?></td>
+    <br>
+<td><b>Telefon:</b></td>
+     <td><?php echo $_POST["tel"];?></td>
+<br>
+
+<td><b>E-Posta Adrisiniz:</b></td>
+     <td><?php echo $_POST["email"];?></td>
+<br>
+<td><b>Mesajınız</b></td><br>
+     <td><?php echo $_POST["mesaj"];?></td>
+    
+
+</body>
+</html>
